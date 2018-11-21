3e41c6d628882d77ae2a89ba05f3dac158662784
SilencerWeb <silencer.web@gmail.com>
2018-11-21 15:22:14 +0500

    Add parsing rules' options; Add dependency eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
+const CLIEngine = require('eslint').CLIEngine;
 
 const {
   prettifyCodeExampleString,
   prettifyShortDescription,
   prettifyLongDescription,
+
+  getRuleOptions,
 } = require('./utils');
 
 
 const runScript = async() => {
+  const cliRules = new CLIEngine().getRules();
+
   const rulesPageResponse = await axios.get('https://eslint.org/docs/rules/');
   const rulesPageDOM = new JSDOM(rulesPageResponse.data);
 
@@ -18,105 +23,61 @@ const runScript = async() => {
   const ruleLists = [...rulesPageDOM.window.document.querySelectorAll('.rule-list')].splice(0, 7); // We don't need two latest lists: list of deprecated rules and list of removed rules
   const rules = [];
 
-  ruleLists.forEach((ruleList, i) => {
-    const DOMRules = [...ruleList.querySelectorAll('tr')];
+  ruleLists.forEach((ruleList) => rules.push(...ruleList.querySelectorAll('tr')));
 
-    let category = null;
-    switch (i) {
-      case 0:
-        category = 'Possible Errors';
-        break;
-      case 1:
-        category = 'Best Practices';
-        break;
-      case 2 :
-        category = 'Strict Mode';
-        break;
-      case 3:
-        category = 'Variables';
-        break;
-      case 4:
-        category = 'Node.js and CommonJS';
-        break;
-      case 5:
-        category = 'Stylistic Issues';
-        break;
-      case 6:
-        category = 'ECMAScript 6';
-        break;
-    }
+  const rulePagesResponses = await Promise.all(rules.map((rule) => axios.get(`https://eslint.org/docs/rules/${rule.querySelector('td p a').textContent}`)));
 
-    rules.push(...DOMRules.map((DOMRule) => ({
-      category: category,
-      element: DOMRule,
-    })));
-  });
+  rulePagesResponses.forEach((rulePagesResponse, i) => {
+    const rule = rules[i];
+    const rulePageDom = new JSDOM(rulePagesResponse.data);
 
-  let index = 1;
-
-  await Promise.all(rules.map(async(rule, i) => {
-    const rulePageResponse = await axios.get(`https://eslint.org/docs/rules/${rule.element.querySelector('td p a').textContent}`);
-    const rulePageDom = new JSDOM(rulePageResponse.data);
-
-    const nameSelector = rule.element.querySelector('td p a');
-    const shortDescriptionSelector = rule.element.querySelector('td:last-child p');
+    const nameSelector = rule.querySelector('td p a');
+    const shortDescriptionSelector = rule.querySelector('td:last-child p');
     const longDescriptionSelector = rulePageDom.window.document.querySelector('#rule-details + p');
     const correctExampleSelector = rulePageDom.window.document.querySelector('.correct + .language-js code');
     const incorrectExampleSelector = rulePageDom.window.document.querySelector('.incorrect + .language-js code');
 
+    const ruleName = nameSelector && nameSelector.textContent;
+
+    const cliRule = cliRules.get(ruleName);
+
     const parsedRule = {
-      index: i, // We need it for sorting, after this we will get rid of it
-      name: nameSelector && nameSelector.textContent,
+      name: ruleName,
+      options: cliRule.meta.schema.length > 0 ? getRuleOptions(cliRule.meta.schema, ruleName) : null,
       value: 'warn',
-      category: rule.category,
+      category: cliRule.meta.docs.category,
       shortDescription: shortDescriptionSelector && prettifyShortDescription(shortDescriptionSelector.textContent),
       longDescription: longDescriptionSelector && prettifyLongDescription(longDescriptionSelector.textContent),
       examples: {
         correct: correctExampleSelector ? prettifyCodeExampleString(correctExampleSelector.innerHTML) : 'No example :(',
         incorrect: incorrectExampleSelector ? prettifyCodeExampleString(incorrectExampleSelector.innerHTML) : 'No example :(',
       },
-      isActive: i === 1, // First element should be active
-      isRecommended: !!rule.element.querySelector('span[title="recommended"]'),
-      isFixable: !!rule.element.querySelector('span[title="fixable"]'),
+      isActive: i === 0, // First element should be active
+      isRecommended: !!rule.querySelector('span[title="recommended"]'),
+      isFixable: !!rule.querySelector('span[title="fixable"]'),
       isTurnedOn: false,
     };
 
-    if (!parsedRule.name) {
-      console.log(`No name presented for the rule ${parsedRule.name}`);
-    }
-
     if (!parsedRule.shortDescription) {
-      console.log(`No short description presented for the rule ${parsedRule.name}`);
+      console.log(`No short description presented for the rule ${ruleName}`);
     }
 
     if (!parsedRule.longDescription) {
-      console.log(`No long description presented for the rule ${parsedRule.name}`);
+      console.log(`No long description presented for the rule ${ruleName}`);
     }
 
     if (parsedRule.examples.correct === 'No example :(') {
-      console.log(`No correct example presented for the rule ${parsedRule.name}`);
+      console.log(`No correct example presented for the rule ${ruleName}`);
     }
 
     if (parsedRule.examples.incorrect === 'No example :(') {
-      console.log(`No incorrect example presented for the rule ${parsedRule.name}`);
+      console.log(`No incorrect example presented for the rule ${ruleName}`);
     }
 
     parsedRules.push(parsedRule);
-
-    console.log(`(${index}/${rules.length}) Finished fetching data for the rule ${nameSelector}`);
-
-    index++;
-  }));
-
-  const sortedParsedRules = parsedRules.sort((rule1, rule2) => rule1.index - rule2.index).map((rule) => {
-    rule = { ...rule };
-
-    delete rule.index;
-
-    return rule;
   });
 
-  fs.writeFile('rules.js', 'export const rules = ' + JSON.stringify(sortedParsedRules), function (error) {
+  fs.writeFile('rules.js', 'export const rules = ' + JSON.stringify(parsedRules), function (error) {
     if (error) {
       throw new Error(error.message);
     }
--- src/utils.js
+++ src/utils.js
@@ -27,8 +27,64 @@ const prettifyLongDescription = (string) => {
 };
 
 
+const getRuleOptions = (options, ruleName) => {
+  const ruleOptions = [];
+
+  options.forEach((option) => {
+    if (option.enum) {
+      const properties = option.enum;
+
+      ruleOptions.unshift({
+        type: 'select',
+        defaultValue: properties[0],
+        value: properties[0],
+        options: properties,
+      });
+    } else if (option.type === 'object') {
+      const propertiesNames = Object.keys(option.properties);
+
+      propertiesNames.forEach((propertyName) => {
+        const property = option.properties[propertyName];
+
+        if (property.type === 'boolean') {
+          ruleOptions.push({
+            name: propertyName,
+            type: 'boolean',
+            defaultValue: false,
+            value: false,
+          });
+        } else if (property.type === 'object' && property.properties) {
+          const properties = Object.keys(property.properties);
+
+          ruleOptions.push({
+            name: propertyName,
+            type: 'select',
+            defaultValue: properties[0],
+            value: properties[0],
+            options: properties,
+          });
+        } else if (property.type === 'string') {
+          ruleOptions.push({
+            name: propertyName,
+            type: 'string',
+            defaultValue: '',
+            value: '',
+          });
+        } else {
+          console.log(`property ${propertyName} in the rule ${ruleName} was not parsed :(`);
+        }
+      });
+    }
+  });
+
+  return ruleOptions;
+};
+
+
 module.exports = {
   prettifyCodeExampleString,
   prettifyShortDescription,
   prettifyLongDescription,
+
+  getRuleOptions,
 };
